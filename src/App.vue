a58e4eacdcdbf5f7f672b43fae6bd8c0dd4a4dad
M_DoubleQ <631127115qq@gmail.com>
2023-05-02 18:48:35 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="App">
+    <h1>App</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped>
+.App {
+  color: #000;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'normalize.css'
+import '@/assets/CSS/index.less'
+createApp(App).mount('#app')
